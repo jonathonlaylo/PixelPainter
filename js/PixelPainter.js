f93138c6a44a7f6b32bb0f41945a243f362d095c
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-11-26 09:04:14 -1000

    made changes to CSS

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,29 +1,42 @@
-//function pixelPainter (width, height) {
-  var pixelPainterCanvas = document.createElement('div');
-  var colorSwatchesCanvas = document.createElement('div');
-  var pixelPainter = document.createElement('div');
-  var whenClicked = null;
-  var color = ['262c04', 'ffc0cb', 'f2df4f', 'eeeeee', 'c6e2ff', '4169e1', '3b411d', 'f10714', '0d8163', '255083', '5f4236', '3496fa', 'fa02d4', '3ff206', '560e3f', '00fa9a', 'violet', '8b0000', 'ff6666', 'c6e2ff', 'd3ffce', '7ffd4', 'b0e0e6', 'b6bb54', 'aa3b4e', 'b08125', 'orange', 'blue', 'purple', 'gray', 'black', 'teal', 'turquoise'];
+var pixelPainter = document.createElement('div');
+var pixelPainterCanvas = document.createElement('div');
+var colorSwatchesCanvas = document.createElement('div');
+var eraseButton = document.createElement('button');
+var clearButton = document.createElement('button');
 
-  pixelPainter.id = 'palette';
-  document.body.appendChild(pixelPainter);
+var whenClicked = null;
+var color = ['262c04', 'ffc0cb', 'f2df4f', 'eeeeee', 'c6e2ff', '4169e1', '3b411d', 'f10714', '0d8163', '255083', '5f4236', '3496fa', 'fa02d4', '3ff206', '560e3f', '00fa9a', 'violet', '8b0000',
+'ff6666', '000080', 'd3ffce', '7ffd4', 'b0e0e6', 'b6bb54', 'aa3b4e', 'b08125', 'orange', 'blue', 'purple', 'gray', 'black', 'teal', 'turquoise', 'red', 'yellow', 'green', '8A2BE2', 'gold', '00FFFF', 'ADFF2F', 'DC143C', '90EE90', '4B0082', 'FF4500'];
 
-  pixelPainterCanvas.className = 'ppCanvas';
-  pixelPainter.appendChild(pixelPainterCanvas);
+pixelPainter.id = 'palette';
+document.body.appendChild(pixelPainter);
 
-  colorSwatchesCanvas.className = 'colorCanvas';
-  pixelPainter.appendChild(colorSwatchesCanvas);
+colorSwatchesCanvas.className = 'colorCanvas';
+pixelPainter.appendChild(colorSwatchesCanvas);
 
-  //Swatch Canvas
-  function swCanvas(pixelSize) {
-    for (var x = 0; x < pixelSize; x++) {
-      var swatchCanvasPixel = document.createElement('div');
-      swatchCanvasPixel.className = 'swatchCanvasPixel';
-      colorSwatchesCanvas.appendChild(swatchCanvasPixel);
-      swatchCanvasPixel.style.backgroundColor = color[x];
-      swatchCanvasPixel.addEventListener('click', storeColorPicker);
-    }
+eraseButton.className = 'buttons';
+eraseButton.innerHTML = "Erase";
+pixelPainter.appendChild(eraseButton);
+eraseButton.addEventListener('click', erase);
+
+clearButton.className = 'buttons';
+clearButton.innerHTML = "Clear";
+pixelPainter.appendChild(clearButton);
+clearButton.addEventListener('click', clear);
+
+pixelPainterCanvas.className = 'ppCanvas';
+pixelPainter.appendChild(pixelPainterCanvas);
+
+//Swatch Canvas
+function swCanvas(pixelSize) {
+  for (var x = 0; x < pixelSize; x++) {
+    var swatchCanvasPixel = document.createElement('div');
+    swatchCanvasPixel.className = 'swatchCanvasPixel';
+    colorSwatchesCanvas.appendChild(swatchCanvasPixel);
+    swatchCanvasPixel.style.backgroundColor = color[x];
+    swatchCanvasPixel.addEventListener('click', storeColorPicker);
   }
+}
 
   //Pixel Painter Canvas
   function ppCanvas(pixelSize) {
@@ -42,14 +55,13 @@
     }
   }
 
-  // colorPick is supposed to click the color from the swatches and store that vaule
-  function storeColorPicker(event) {
-    currentColor = event.target.style.backgroundColor;
-    console.log(event);
-  }
+// colorPick is supposed to click the color from the swatches and store that vaule
+function storeColorPicker(event) {
+  currentColor = event.target.style.backgroundColor;
+}
 
 ppCanvas(2500);
-swCanvas(10);
+swCanvas(44);
 
 function storeColorPicker(event) {
   currentColor = event.target.style.backgroundColor;
@@ -89,18 +101,6 @@ function colorContinous(){
     event.target.style.backgroundColor = currentColor;
   }
 }
-
-var eraseButton = document.createElement('button');
-eraseButton.className = 'buttons';
-eraseButton.innerHTML = "Erase";
-pixelPainter.appendChild(eraseButton);
-eraseButton.addEventListener('click', erase);
-
-var clearButton = document.createElement('button');
-clearButton.className = 'buttons';
-clearButton.innerHTML = "Clear";
-pixelPainter.appendChild(clearButton);
-clearButton.addEventListener('click', clear);
 
 function erase(event) {
   currentColor = 'white';
